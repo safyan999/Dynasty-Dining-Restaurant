14102029b67c6006c3f6b96322197595e0d247b5
=safyan999 <=safyantahir780@gmail.com>
2024-03-19 21:25:43 +0500

    dynstyDinning Project created

---
+++ dynasty_dining_restaurant/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <h1> hello new project</h1>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
