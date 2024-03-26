15e4dd01fe2a66c6506295561f477965f17a3e43
=safyan999 <=safyantahir780@gmail.com>
2024-03-27 02:52:18 +0500

    add team member profile in about us and fix about us page

--- dynasty_dining_restaurant/src/components/About.vue
+++ dynasty_dining_restaurant/src/components/About.vue
@@ -4,47 +4,213 @@
     <MyNavbar />
     <div class="hero2">
         <h1><b>About Us</b></h1>
-        <a href="#">HOME</a>/
-        <a href="#">PAGE</a>/
+        <a href="#">HOME</a>/ <a href="#">PAGE</a>/
         <a href="#">ABOUT</a>
     </div>
 </div>
-
+<!-- hero main content started -->
 <div class="maincontent container p-5">
     <div class="row g-5 align-items-center justify-content-center">
         <div class="col-lg-6">
             <div class="row g-3">
                 <div class="col-6 text-start">
-                    <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.1s" src="../assets/about-1.jpg">
+                    <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.1s" src="../assets/about-1.jpg" />
                 </div>
                 <div class="col-6 text-start">
-                    <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.3s" src="../assets/about-2.jpg" style="margin-top: 25%;">
+                    <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.3s" src="../assets/about-2.jpg" style="margin-top: 25%" />
                 </div>
                 <div class="col-6 text-end">
-                    <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.5s" src="../assets/about-3.jpg">
+                    <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.5s" src="../assets/about-3.jpg" />
                 </div>
                 <div class="col-6 text-end">
-                    <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.7s" src="../assets/about-4.jpg">
+                    <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.7s" src="../assets/about-4.jpg" />
                 </div>
             </div>
         </div>
         <div class="col-lg-6 row2">
-            <h5 class="section-title ff-secondary text-start text-primary fw-normal"> About US</h5>
-            <h1 class="mb-4">welcome to <i class="fa fa-utensils  text-primary me-2"></i>Restoran </h1>
+            <h5 class="section-title ff-secondary text-start text-primary fw-normal">
+                About US
+            </h5>
+            <h1 class="mb-4">
+                welcome to <i class="fa fa-utensils text-primary me-2"></i>Restoran
+            </h1>
 
             <p class="mb-4">
-                Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos erat ipsum et lorem et sit, sed stet lorem sit.
-
+                Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu
+                diam amet diam et eos erat ipsum et lorem et sit, sed stet lorem sit.
             </p>
             <p class="mb-4">
-                Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet
-
+                Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu
+                diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet
+                lorem sit clita duo justo magna dolore erat amet
             </p>
+            <div class="row g-4">
+                <div class="col-sm-6">
+                    <div class="d-flex align-items-center border-start border-5 px-3 border-primary">
+                        <h1 class="flex-shrink-0 display-5 text-primary mb-0" data-toggle="-up">
+                            15
+                        </h1>
+                        <div class="ps-4">
+                            <p class="mb-0">Years of</p>
+                            <h6 class="text-uppercase mb-0">Experience</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="d-flex align-items-center border-start border-5 px-3 border-primary">
+                        <h1 class="flex-shrink-0 display-5 text-primary mb-0" data-toggle="-up">
+                            50
+                        </h1>
+                        <div class="ps-4">
+                            <p class="mb-0">Popular</p>
+                            <h6 class="text-uppercase mb-0">master chefs</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="btn btn-primary py-3 px-5 mt-4" href="#"> Read more</a>
         </div>
     </div>
 </div>
+<!-- hero manin content ended -->
 
-<h1 class="mt-5">Hello About</h1>
+<!-- our Team chied started  -->
+<div class="container p-5  hero2 text-primary">
+    <div class="ourteam ">
+        <h5 class="section-title  text-primary fw-normal">
+            Team memebers
+        </h5>
+        <h1 class="mb-4">
+            Our Master Chefs
+        </h1>
+        <div class="row">
+            <div class="col-lg-3 col-sm-6 mb-4">
+                <div class="card p-0">
+                    <div class="card-image">
+                        <img src="../assets/team-1.jpg" alt="">
+                    </div>
+                    <div class="card-content d-flex flex-column align-items-center">
+                        <h4 class="pt-2">Heston Blumenthal</h4>
+                        <h5>bacon-and-egg ice cream.</h5>
+
+                        <ul class="social-icons d-flex justify-content-center">
+                            <li style="--i:1">
+                                <a href="#">
+                                    <span class="fab fa-facebook"></span>
+                                </a>
+                            </li>
+                            <li style="--i:2">
+                                <a href="#">
+                                    <span class="fab fa-twitter"></span>
+                                </a>
+                            </li>
+                            <li style="--i:3">
+                                <a href="#">
+                                    <span class="fab fa-instagram"></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card p-0">
+                    <div class="card-image">
+                        <img src="../assets/team-2.jpg" alt="">
+                    </div>
+                    <div class="card-content d-flex flex-column align-items-center">
+                        <h4 class="pt-2">Heston Blumenthal</h4>
+                        <h5>bacon-and-egg ice cream.</h5>
+
+                        <ul class="social-icons d-flex justify-content-center">
+                            <li style="--i:1">
+                                <a href="#">
+                                    <span class="fab fa-facebook"></span>
+                                </a>
+                            </li>
+                            <li style="--i:2">
+                                <a href="#">
+                                    <span class="fab fa-twitter"></span>
+                                </a>
+                            </li>
+                            <li style="--i:3">
+                                <a href="#">
+                                    <span class="fab fa-instagram"></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card p-0">
+                    <div class="card-image">
+                        <img src="../assets/team-3.jpg" alt="">
+                    </div>
+                    <div class="card-content d-flex flex-column align-items-center">
+                        <h4 class="pt-2">Heston Blumenthal</h4>
+                        <h5>bacon-and-egg ice cream.</h5>
+
+                        <ul class="social-icons d-flex justify-content-center">
+                            <li style="--i:1">
+                                <a href="#">
+                                    <span class="fab fa-facebook"></span>
+                                </a>
+                            </li>
+                            <li style="--i:2">
+                                <a href="#">
+                                    <span class="fab fa-twitter"></span>
+                                </a>
+                            </li>
+                            <li style="--i:3">
+                                <a href="#">
+                                    <span class="fab fa-instagram"></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card p-0">
+                    <div class="card-image">
+                        <img src="../assets/team-4.jpg" alt="">
+                    </div>
+                    <div class="card-content d-flex flex-column align-items-center">
+                        <h4 class="pt-2">Heston Blumenthal</h4>
+                        <h5>bacon-and-egg ice cream.</h5>
+
+                        <ul class="social-icons d-flex justify-content-center">
+                            <li style="--i:1">
+                                <a href="#">
+                                    <span class="fab fa-facebook"></span>
+                                </a>
+                            </li>
+                            <li style="--i:2">
+                                <a href="#">
+                                    <span class="fab fa-twitter"></span>
+                                </a>
+                            </li>
+                            <li style="--i:3">
+                                <a href="#">
+                                    <span class="fab fa-instagram"></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+           
+            
+
+        </div>
+
+    </div>
+</div>
 </template>
 
 <script>
@@ -59,6 +225,18 @@ export default {
 </script>
 
 <style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+}
+
+body {
+    min-height: 100vh;
+    background: linear-gradient(to bottom, #000428, #004683);
+}
+
 .hero1 {
     background-image: url("../assets/bg-hero.jpg");
     background-repeat: no-repeat;
@@ -67,7 +245,7 @@ export default {
 .hero2 {
     text-align: center;
     padding: 100px;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
     font-size: larger;
     color: white;
     font-weight: bold;
@@ -92,5 +270,107 @@ export default {
     -webkit-transform: scale(0.5);
     /* Safari 3-8 */
     transform: scale(0.5);
+}
+
+card {
+    position: relative;
+    padding: 0;
+    margin: 0 !important;
+    border-radius: 20px;
+    overflow: hidden;
+    max-width: 280px;
+    max-height: 340px;
+    cursor: pointer;
+    border: none;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+
+}
+
+.card .card-image {
+    width: 100%;
+    max-height: 340px;
+}
+
+.card .card-image img {
+    width: 100%;
+    max-height: 340px;
+    object-fit: cover;
+}
+
+.card .card-content {
+    position: absolute;
+    bottom: -180px;
+    color: #fff;
+    background: rgba(255, 255, 255, 0.2);
+    backdrop-filter: blur(15px);
+    min-height: 140px;
+    width: 100%;
+    transition: bottom .4s ease-in;
+    box-shadow: 0 -10px 10px rgba(255, 255, 255, 0.1);
+    border-top: 1px solid rgba(255, 255, 255, 0.2);
+}
+
+.card:hover .card-content {
+    bottom: 0px;
+}
+
+.card:hover .card-content h4,
+.card:hover .card-content h5 {
+    transform: translateY(10px);
+    /* opacity: 1; */
+}
+
+.card .card-content h4,
+.card .card-content h5 {
+    font-size: 1.1rem;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    text-align: center;
+    transition: 0.8s;
+    font-weight: 500;
+    /* opacity: 0; */
+    transform: translateY(-40px);
+    transition-delay: 0.2s;
+}
+
+.card .card-content h5 {
+    transition: 0.5s;
+    font-weight: 200;
+    font-size: 0.8rem;
+    letter-spacing: 2px;
+}
+
+.card .card-content .social-icons {
+    list-style: none;
+    padding: 0;
+}
+
+.card .card-content .social-icons li {
+    margin: 10px;
+    transition: 0.5s;
+    transition-delay: calc(0.15s * var(--i));
+    transform: translateY(50px);
+}
+
+.card:hover .card-content .social-icons li {
+    transform: translateY(20px);
+}
+
+.card .card-content .social-icons li a {
+    color: #fff;
+}
+
+.card .card-content .social-icons li a span {
+    font-size: 1.3rem;
+}
+
+@media(max-width: 991.5px) {
+    .container {
+        margin-top: 20px;
+    }
+
+    .container .row .col-lg-4 {
+        margin: 20px 0px;
+    }
 }
 </style>
